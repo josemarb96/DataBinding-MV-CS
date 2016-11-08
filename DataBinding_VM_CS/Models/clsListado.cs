96a6549c9d4b372c231506c329b9d8f9fe0471f2
josemarb96 <josemarb96@gmail.com>
2016-11-08 13:19:37 +0100

    DataBinding MV C#
    En el ListView haremos un binding para meter las personas de nuestro
    listado.

---
+++ DataBinding_VM_CS/Models/clsListado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataBinding_VM_CS.Models;
+using System.Collections.ObjectModel;
+
+namespace DataBinding_VM_CS.Models
+{
+    public class clsListado
+    {
+        public ObservableCollection<clsPersona> getListadoPersonas()
+        {
+            ObservableCollection<clsPersona> lista = new ObservableCollection<clsPersona>();
+            
+            lista.Add(new clsPersona(1,"David", "Benítez Rasero", new DateTime(2012,12,12), "Montequinto", "123456789"));
+            lista.Add(new clsPersona(2,"Makoto", "Naegi", new DateTime(1996, 12, 8), "Japon Street", "123789456"));
+            lista.Add(new clsPersona(3,"Sayaka", "Maizono", new DateTime(1999, 4, 25), "Quinto Pino, Pinar 12", "147258369"));
+            lista.Add(new clsPersona(4,"Kyouko", "Kirigiri", new DateTime(2015, 7, 7), "Av. de la Constitucion Nº12", "963852147"));
+
+            return lista;
+        }
+    }
+}
---
+++ DataBinding_VM_CS/ViewModels/clsMainPageVM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using DataBinding_VM_CS.Models;
+
+namespace DataBinding_VM_CS.ViewModels
+{
+    public class clsMainPageVM
+    {
+        private clsPersona _personaSeleccionada;
+        private ObservableCollection<clsPersona> _listado;
+
+        public clsPersona personaSeleccionada
+        {
+            get
+            {
+                return _personaSeleccionada;
+            }
+            set
+            {
+                _personaSeleccionada = value;
+            }
+        }
+        public ObservableCollection<clsPersona> listado
+        {
+            get
+            {
+                return _listado;
+            }            
+        }
+        public clsMainPageVM()
+        {
+            clsListado lista = new clsListado();
+            _listado = lista.getListadoPersonas();
+        }
+       
+    }
+}
